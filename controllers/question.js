67e880382c2f3530d3bd97985e04978754e2760e
Toby Alden <toby.alden@gmail.com>
2015-06-12 10:01:40 -0700

    Add edit description feature to question page (changes are lost on browser reload).

--- controllers/question.js
+++ controllers/question.js
@@ -1,6 +1,16 @@
 MessageBoard.QuestionController = Ember.ObjectController.extend({
 
+  isEditing: false,
+
   actions: {
+
+    editDescription: function() {
+      this.set('isEditing', true);
+    },
+
+    finishEditing: function() {
+      this.set('isEditing', false);
+    }
 
   }
 });
